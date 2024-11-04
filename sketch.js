a26af8fd5274f26393a3d810145bdbe718faa704
GuzmaChrono <yandeimperio@gmail.com>
2024-11-04 12:20:18 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,40 @@
+function setup() {
+    createCanvas(400, 400);
+  }
+  
+  function draw() {
+    background("blue");
+    rect(0, 0, 100, 150);
+    fill ("red");
+    rect(mouseX, mouseY, 20, 35);
+   // console.log( mouseIsPressed)
+    If (mouseIsPressed)
+  }
+  function setup() {
+    createCanvas(400, 400);
+    background("white")
+  }
+  
+  function draw() {
+    stroke("blue");function setup() {
+    createCanvas(400, 400);
+  }
+  
+  function draw() {
+    background("blue");
+    rect(0, 0, 100, 150);
+    fill ("red");
+    rect(mouseX, mouseY, 20, 35);
+   // console.log( mouseIsPressed)
+    If (mouseIsPressed)
+  }
+  function setup() {
+    createCanvas(400, 400);
+    fill("red");
+    
+    
+    if (mouseIsPressed) {
+      rect(mouseX, mouseY, 20, 35);
+    }
+  }
+  
